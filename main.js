4d5639c0db9d155bce1d939edc1759dfebedd86f
Rayhan1998 <miahrayhan01@gmail.com>
2020-06-15 19:46:17 -0400

    added javascipt moudle patterns

--- main.js
+++ main.js
@@ -1,1 +1,59 @@
-console.log("hello");
+var filterInput = document.querySelector(".filterTodo");
+var addTodo = document.querySelector(".addTodo");
+var btn = document.querySelector(".button");
+
+var listController = (function() {
+  var searchList = {
+    filterdSearch: ""
+  };
+  var todoList = [
+    {
+      todo: "running",
+      comepleted: true
+    },
+
+    {
+      todo: "shop",
+      comepleted: false
+    }
+  ];
+
+  return {
+    getTodoList: function() {
+      return todoList;
+    },
+
+    getSearchList: function() {
+      return searchList;
+    }
+  };
+})();
+
+var UIController = (function() {
+  return {
+    clearInputs: function() {
+      addTodo.value = "";
+    }
+  };
+})();
+
+var appController = (function(UIctrl, listCtrl) {
+  var renderdTodos = function(todoList, filter) {};
+
+  filterInput.addEventListener("input", function(e) {
+    console.log(e);
+  });
+
+  document.addEventListener("keypress", function(e) {
+    if (e.keyCode === 13) {
+      readTodoValue();
+    }
+  });
+
+  btn.addEventListener("click", readTodoValue);
+
+  function readTodoValue() {
+    console.log(addTodo.value);
+    UIController.clearInputs();
+  }
+})(UIController, listController);
